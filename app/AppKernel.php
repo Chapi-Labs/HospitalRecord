a2177c98e0071e9dc7973b57e152edeb7662c273
Pablo Díaz Márquez <pablo5_diaz@hotmail.com>
2015-12-14 16:35:57 -0600

    configuraciones iniciales

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,13 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
             new AppBundle\AppBundle(),
+            new UserBundle\UserBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
